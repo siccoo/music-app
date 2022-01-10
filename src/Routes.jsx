52f0e6414b52c1e53399a7c3eea9bd1bf201a69a
Chilaka Michael Obinna <obichilaka@gmail.com>
2022-01-10 21:16:23 +0100

    files & folder structure

---
+++ src/Routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import HomePage from './pages/Home/HomePage';
+// import { Switch, Route } from 'react-router-dom';
+
+const Routes = () => (
+    // <Switch>
+    //     {/* Render all page and component routes here */}
+    //     {/* <Route exact path="/" component={HomePage} /> */}
+    // </Switch>
+    <div> </div>
+)
+
+export default Routes;
