a0b4ca7e8d0f804cc11ed238b96b225cf5bbc309
Chilaka Michael Obinna <obichilaka@gmail.com>
2022-01-11 11:06:11 +0100

    updates on Navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../../assets/images/radio-logo.svg";
+
+const Navbar = () => {
+  return (
+    <section className="music">
+      <div className="music__navbar">
+        <div className="music__navbar-links"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
