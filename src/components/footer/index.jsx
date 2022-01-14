c1c64f68ad99f0ff6987e17b4294060ed11343bb
Chilaka Michael Obinna <obichilaka@gmail.com>
2022-01-14 06:24:06 +0100

    updated footer section

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React from "react";
 
 import "./footer.scss";
 
+import Logo from "../../assets/radio-logo.svg";
+
 const Footer = () => {
-    return (
-        <div>
-            Footer
+  return (
+    <div>
+      <section className="footer__section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <img src={Logo} alt="radiohead" />
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+      <section className="footer__section__dark">
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+            <ul class="list-unstyled list-inline social text-center">
+              <li class="list-inline-item">
+                {" "}
+                <a href="#">
+                  {" "}
+                  <i class="fa fa-facebook"> </i>{" "}
+                </a>{" "}
+              </li>
+              <li class="list-inline-item">
+                {" "}
+                <a href="#">
+                  {" "}
+                  <i class="fa fa-twitter"> </i>{" "}
+                </a>{" "}
+              </li>
+              <li class="list-inline-item">
+                {" "}
+                <a href="#">
+                  {" "}
+                  <i class="fa fa-instagram"> </i>{" "}
+                </a>{" "}
+              </li>
+              <li class="list-inline-item">
+                {" "}
+                <a href="#">
+                  {" "}
+                  <i class="fa fa-google-plus"> </i>{" "}
+                </a>{" "}
+              </li>
+              <li class="list-inline-item">
+                {" "}
+                <a href="#">
+                  {" "}
+                  <i class="fa fa-envelope"> </i>{" "}
+                </a>{" "}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Footer;
