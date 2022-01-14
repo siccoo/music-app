9a47621c48863bad512d6dcaadee0cf159ab09c1
Chilaka Michael Obinna <obichilaka@gmail.com>
2022-01-14 03:33:56 +0100

    brands & discover containers added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Navbar, Footer } from "../../components";
 import { Brands, Discover, Music, Playlist, Why } from "../../containers";
 
-import "./Home.scss";
-
 const Home = () => {
   return (
     <div>
